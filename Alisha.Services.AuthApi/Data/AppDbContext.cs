7d5e6a7f165bd7b82b2f798d70f430256aa06282
Rumi Perween <Rumi.perween312@gmail.com>
2026-01-03 12:01:49 +0530

    Add project files.

---
+++ Alisha.Services.AuthApi/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Alisha.Services.AuthApi.Models;
+using Alisha.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alisha.Services.AuthAPI.Data
+{
+
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Alisha.Services.AuthApi/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Alisha.Services.AuthApi.Models;
+
+namespace Alisha.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles);
+    }
+}
